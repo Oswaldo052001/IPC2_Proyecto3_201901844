b09fc5c3dd0bfd938c2db290ab56d13cebdb348d
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-10-16 23:53:35 -0600

    Avance en el frontend

--- Front/Front/urls.py
+++ Front/Front/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Front.view import getInfoEstudiante, reporte
+from Front.view import inicio,enlace1, reporte
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('getInfo/', getInfoEstudiante),
-    path('reporte/',reporte),
+    path('', inicio),
+    path('enlace1/',enlace1)
+    #path('grafica/', grafica),
+    #path('reporte/',reporte),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- Front/Front/view.py
+++ Front/Front/view.py
@@ -2,20 +2,30 @@ from django.http import HttpResponse
 from django.template import loader
 from requests import post, get
 
-def getInfoEstudiante(request):
-    return HttpResponse("Frontend: Estudiante de IPC2")
+def inicio(request):
+    objtmp = loader.get_template("index.html")
+    html = objtmp.render()
+    return HttpResponse(html)
+
+
+def enlace1(request):
+    objtmp = loader.get_template("Pagina1.html")
+    html = objtmp.render()
+    return HttpResponse(html)
+
 
 def reporte(reques):
 
-    url = "http://127.0.0.1:3050/"
-    respuesta = get(url)
-    print(respuesta.json())
+    #url = "http://127.0.0.1:3050/"
+    #respuesta = get(url)
+    #print(respuesta.json())
 
     existe = False
     namereporte =  "Sentimiento de mensaje"
     descripcion = "Reporte de sentimientos de mensaje"
     conteo = [10,20,50]
 
-    objtmp = loader.get_template("reporte.html")
-    html = objtmp.render({"existe":existe,"respue":respuesta.json(),"idNombre_reporte":namereporte, "descripcion":descripcion, "valores":conteo})
+    objtmp = loader.get_template("index.html")
+    #"respue":respuesta.json()
+    html = objtmp.render({"existe":existe,"idNombre_reporte":namereporte, "descripcion":descripcion, "valores":conteo})
     return HttpResponse(html)
